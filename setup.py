7628ef384044dd279073cbe8dfd85e8a903ddd65
PhilipGarnero <philip.garnero@gmail.com>
2015-08-09 13:57:34 +0200

    update django-oauth-toolkit to version 0.9.0 in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     ],
     install_requires=[
         'djangorestframework>=3.0.1',
-        'django-oauth-toolkit>=0.7.2',
+        'django-oauth-toolkit>=0.9.0',
         'python-social-auth>=0.2.2'
     ],
     include_package_data=True,
