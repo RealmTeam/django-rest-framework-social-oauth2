57a3870f6e4400461281bbcac6f9513bb662d564
PhilipGarnero <philip.garnero@gmail.com>
2015-08-11 23:23:51 +0200

    fixed a bug where the keep django request hack was not working

--- rest_framework_social_oauth2/oauth2_endpoints.py
+++ rest_framework_social_oauth2/oauth2_endpoints.py
@@ -2,10 +2,16 @@
 
 from __future__ import unicode_literals
 
+import logging
+
+from oauthlib.common import Request
 from oauthlib.oauth2.rfc6749.endpoints.token import TokenEndpoint
 from oauthlib.oauth2.rfc6749.tokens import BearerToken
+from oauthlib.oauth2.rfc6749.endpoints.base import catch_errors_and_unavailability
 
 from .oauth2_grants import SocialTokenGrant
+
+log = logging.getLogger(__name__)
 
 
 class SocialTokenServer(TokenEndpoint):
@@ -34,3 +40,21 @@ class SocialTokenServer(TokenEndpoint):
                                    'convert_token': refresh_grant,
                                },
                                default_token_type=bearer)
+
+    # We override this method just so we can pass the django request object
+    @catch_errors_and_unavailability
+    def create_token_response(self, uri, http_method='GET', body=None,
+                              headers=None, credentials=None):
+        """Extract grant_type and route to the designated handler."""
+        django_request = headers.pop("Django-request-object", None)
+        request = Request(
+            uri, http_method=http_method, body=body, headers=headers)
+        request.scopes = None
+        request.extra_credentials = credentials
+        request.django_request = django_request
+        grant_type_handler = self.grant_types.get(request.grant_type,
+                                                  self.default_grant_type_handler)
+        log.debug('Dispatching grant_type %s request to %r.',
+                  request.grant_type, grant_type_handler)
+        return grant_type_handler.create_token_response(
+            request, self.default_token_type)
