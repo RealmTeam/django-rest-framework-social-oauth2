53ced0963bb2198dad293f9f326a9a5214ef3f61
deshraj <deshrajdry@gmail.com>
2016-02-05 14:21:15 +0530

    fixes to support django 1.9

--- rest_framework_social_oauth2/urls.py
+++ rest_framework_social_oauth2/urls.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url, include
 from oauth2_provider.views import AuthorizationView, TokenView, RevokeTokenView
 
 from .views import ConvertTokenView, invalidate_sessions
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^authorize/?$', AuthorizationView.as_view(), name="authorize"),
     url(r'^token/?$', TokenView.as_view(), name="token"),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^convert-token/?$', ConvertTokenView.as_view(), name="convert_token"),
     url(r'^revoke-token/?$', RevokeTokenView.as_view(), name="revoke_token"),
     url(r'^invalidate-sessions/?$', invalidate_sessions, name="invalidate_sessions")
-)
+]
