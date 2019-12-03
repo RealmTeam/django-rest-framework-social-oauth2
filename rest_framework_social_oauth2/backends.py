RealmTeam/django-rest-framework-social-oauth2

commit 009adbd8a4bf94eae0c5d0eee75d42af3029751b

Updated python social auth (fixes #78, #77 and #76) Wrapped token views in an APIView (closes #71) Added URL namespace (closes #69) Updated the README (closes #73)

rest_framework_social_oauth2/backends.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from social.backends.oauth import BaseOAuth2
+from social_core.backends.oauth import BaseOAuth2
 from django.core.urlresolvers import reverse
-from .settings import PROPRIETARY_BACKEND_NAME
+from .settings import DRFSO2_PROPRIETARY_BACKEND_NAME, DRFSO2_URL_NAMESPACE
 
 class DjangoOAuth2(BaseOAuth2):
     """Default OAuth2 authentication backend used by this package"""
-    name = PROPRIETARY_BACKEND_NAME
-    AUTHORIZATION_URL = reverse('authorize')
-    ACCESS_TOKEN_URL = reverse('token')
+    name = DRFSO2_PROPRIETARY_BACKEND_NAME
+    AUTHORIZATION_URL = reverse(DRFSO2_URL_NAMESPACE + ':authorize'
+                                if DRFSO2_URL_NAMESPACE else 'authorize')
+    ACCESS_TOKEN_URL = reverse(DRFSO2_URL_NAMESPACE + ':token'
+                               if DRFSO2_URL_NAMESPACE else 'authorize')

rest_framework_social_oauth2/__init__.py
@@ -1,4 +1,4 @@
 """python-social-auth and oauth2 support for django-rest-framework"""
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 

rest_framework_social_oauth2/authentication.py
@@ -5,10 +5,10 @@ from django.core.urlresolvers import reverse
 from rest_framework.authentication import BaseAuthentication, get_authorization_header
 from rest_framework import exceptions, HTTP_HEADER_ENCODING
 
-from social.apps.django_app.views import NAMESPACE
-from social.apps.django_app.utils import load_backend, load_strategy
-from social.exceptions import MissingBackend
-from social.utils import requests
+from social_django.views import NAMESPACE
+from social_django.utils import load_backend, load_strategy
+from social_core.exceptions import MissingBackend
+from social_core.utils import requests
 
 
 class SocialAuthentication(BaseAuthentication):

rest_framework_social_oauth2/oauth2_grants.py
@@ -9,10 +9,10 @@ from django.core.urlresolvers import reverse
 from oauthlib.oauth2.rfc6749 import errors
 from oauthlib.oauth2.rfc6749.grant_types.refresh_token import RefreshTokenGrant
 
-from social.apps.django_app.views import NAMESPACE
-from social.apps.django_app.utils import load_backend, load_strategy
-from social.exceptions import MissingBackend, SocialAuthBaseException
-from social.utils import requests
+from social_django.views import NAMESPACE
+from social_django.utils import load_backend, load_strategy
+from social_core.exceptions import MissingBackend, SocialAuthBaseException
+from social_core.utils import requests
 
 
 log = logging.getLogger(__name__)

rest_framework_social_oauth2/settings.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
-PROPRIETARY_BACKEND_NAME = getattr(settings, 'PROPRIETARY_BACKEND_NAME', "Django")
+DRFSO2_PROPRIETARY_BACKEND_NAME = getattr(settings, 'DRFSO2_PROPRIETARY_BACKEND_NAME', "Django")
+DRFSO2_URL_NAMESPACE = getattr(settings, 'DRFSO2_URL_NAMESPACE', "")

rest_framework_social_oauth2/urls.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url, include
-from oauth2_provider.views import AuthorizationView, TokenView, RevokeTokenView
+from oauth2_provider.views import AuthorizationView
 
-from .views import ConvertTokenView, invalidate_sessions
+from .views import ConvertTokenView, TokenView, RevokeTokenView, invalidate_sessions
 
 urlpatterns = [
     url(r'^authorize/?$', AuthorizationView.as_view(), name="authorize"),
     url(r'^token/?$', TokenView.as_view(), name="token"),
-    url('', include('social.apps.django_app.urls', namespace='social')),
+    url('', include('social_django.urls', namespace="social")),
     url(r'^convert-token/?$', ConvertTokenView.as_view(), name="convert_token"),
     url(r'^revoke-token/?$', RevokeTokenView.as_view(), name="revoke_token"),
     url(r'^invalidate-sessions/?$', invalidate_sessions, name="invalidate_sessions")

rest_framework_social_oauth2/views.py
@@ -2,6 +2,8 @@
 import json
 
 from braces.views import CsrfExemptMixin
+from oauthlib.oauth2.rfc6749.endpoints.token import TokenEndpoint
+from oauth2_provider.oauth2_backends import OAuthLibCore
 from oauth2_provider.ext.rest_framework import OAuth2Authentication
 from oauth2_provider.models import Application, AccessToken
 from oauth2_provider.settings import oauth2_settings
@@ -16,7 +18,7 @@ from .oauth2_backends import KeepRequestCore
 from .oauth2_endpoints import SocialTokenServer
 
 
-class ConvertTokenView(CsrfExemptMixin, OAuthLibMixin, APIView):
+class TokenView(CsrfExemptMixin, OAuthLibMixin, APIView):
     """
     Implements an endpoint to provide access tokens
 
@@ -26,13 +28,12 @@ class ConvertTokenView(CsrfExemptMixin, OAuthLibMixin, APIView):
     * Password
     * Client credentials
     """
-    server_class = SocialTokenServer
+    server_class = oauth2_settings.OAUTH2_SERVER_CLASS
     validator_class = oauth2_settings.OAUTH2_VALIDATOR_CLASS
-    oauthlib_backend_class = KeepRequestCore
+    oauthlib_backend_class = OAuthLibCore
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
-
         # Use the rest framework `.data` to fake the post body of the django request.
         request._request.POST = request._request.POST.copy()
         for key, value in request.data.items():
@@ -40,6 +41,57 @@ class ConvertTokenView(CsrfExemptMixin, OAuthLibMixin, APIView):
 
         url, headers, body, status = self.create_token_response(request._request)
         response = Response(data=json.loads(body), status=status)
+
+        for k, v in headers.items():
+            response[k] = v
+        return response
+
+
+class ConvertTokenView(CsrfExemptMixin, OAuthLibMixin, APIView):
+    """
+    Implements an endpoint to convert a provider token to an access token
+
+    The endpoint is used in the following flows:
+
+    * Authorization code
+    * Client credentials
+    """
+    server_class = SocialTokenServer
+    validator_class = oauth2_settings.OAUTH2_VALIDATOR_CLASS
+    oauthlib_backend_class = KeepRequestCore
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        # Use the rest framework `.data` to fake the post body of the django request.
+        request._request.POST = request._request.POST.copy()
+        for key, value in request.data.items():
+            request._request.POST[key] = value
+
+        url, headers, body, status = self.create_token_response(request._request)
+        response = Response(data=json.loads(body), status=status)
+
+        for k, v in headers.items():
+            response[k] = v
+        return response
+
+
+class RevokeTokenView(CsrfExemptMixin, OAuthLibMixin, APIView):
+    """
+    Implements an endpoint to revoke access or refresh tokens
+    """
+    server_class = oauth2_settings.OAUTH2_SERVER_CLASS
+    validator_class = oauth2_settings.OAUTH2_VALIDATOR_CLASS
+    oauthlib_backend_class = OAuthLibCore
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        # Use the rest framework `.data` to fake the post body of the django request.
+        request._request.POST = request._request.POST.copy()
+        for key, value in request.data.items():
+            request._request.POST[key] = value
+
+        url, headers, body, status = self.create_revocation_response(request._request)
+        response = Response(data=json.loads(body) if body else '', status=status if body else 204)
 
         for k, v in headers.items():
             response[k] = v

setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires=[
         'djangorestframework>=3.0.1',
         'django-oauth-toolkit>=0.9.0',
-        'python-social-auth>=0.2.2'
+        'social-auth-app-django>=0.1.0'
     ],
     include_package_data=True,
     zip_safe=False,

commit 22dd3e6f9d0d4134e64387395be74aea31e2e19f

Fix ACCESS_TOKEN_URL namespace
Fixes a possible bug introduced in 009adbd

rest_framework_social_oauth2/backends.py
@@ -12,4 +12,4 @@ class DjangoOAuth2(BaseOAuth2):
     AUTHORIZATION_URL = reverse(DRFSO2_URL_NAMESPACE + ':authorize'
                                 if DRFSO2_URL_NAMESPACE else 'authorize')
     ACCESS_TOKEN_URL = reverse(DRFSO2_URL_NAMESPACE + ':token'
-                               if DRFSO2_URL_NAMESPACE else 'authorize')
+                               if DRFSO2_URL_NAMESPACE else 'token')